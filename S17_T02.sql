53c80fd9b09f8a4551c2f42e15670939380bf28c
Gabriel De Santis <73543020+dadavalangege@users.noreply.github.com>
2021-10-13 19:02:09 +0200

    Add files via upload

---
+++ S17_T02.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE franquicia;
+SHOW DATABASES;
+USE franquicia;
+CREATE TABLE clients (
+	id_clients int AUTO_INCREMENT,
+    nom VARCHAR(50) NOT NULL,
+    cognom VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id_clients)
+);
+SHOW TABLES;
+DESCRIBE clients;
+CREATE TABLE establiments (
+	id_establiment int AUTO_INCREMENT,
+    nom VARCHAR(50) NOT NULL,
+    loc VARCHAR(100) NOT NULL,
+    superf INT NOT NULL,
+    PRIMARY KEY (id_establiment)
+);
+DESCRIBE establiments;
+CREATE TABLE treballadors (
+	id_treballador int AUTO_INCREMENT,
+    nom VARCHAR(50) NOT NULL,
+    cognom VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id_treballador)
+);
+DESCRIBE treballadors;
+CREATE TABLE productes (
+	id_producte int AUTO_INCREMENT,
+    tipus VARCHAR(100),
+    preu int,
+    PRIMARY KEY(id_producte)
+);
+SHOW TABLES;
